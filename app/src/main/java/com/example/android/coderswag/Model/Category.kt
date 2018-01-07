d4e30311e53156b9135d35d7c1323987706416cc
Rick McDonald <rngmc814@gmail.com>
2018-01-07 14:55:52 -0800

    lesson 02

--- app/src/main/java/com/example/android/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/android/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.android.coderswag.Model
 /**
  * Created by Rick on 1/6/2018.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
